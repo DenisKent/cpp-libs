a6c5055552b47e34a526f599c11266ebffc735d6
DenisKent <kentdenis6@hotmail.co.uk>
2024-12-20 17:59:00 +0000

    chore: example of unordered map usage

--- hashmap/hashmap_test.cpp
+++ hashmap/hashmap_test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 #include "../simple_test.h"
 #include "./hashmap.h"
 
@@ -22,15 +23,31 @@ static void keys_share_hash()
 static void hash_types()
 {
     Hashmap<int> intHash{1};
-    intHash.set("two",2);
-    ASSERT_EQ(intHash.get("two"),2);
+    intHash.set("one",1);
+    ASSERT_EQ(intHash.get("one"),1);
+    Hashmap<std::string> stringHash{1};
+    stringHash.set("two","two");
+    ASSERT_EQ(stringHash.get("two"),"two");
 }
 
+static void similar_to_unordered_map()
+{
+    std::unordered_map<int,int> map{1};
+    int map_key = 1;
+    map.insert({map_key, 2});
+    int result = 0;
+    if(map.find(map_key) != map.end()){
+        result = map[map_key];
+    }
+    ASSERT_EQ(result, 2);
+}
 
 int main()
 {
     ADD_TEST("can overwrite keys", overwrite_key);
     ADD_TEST("handles keys that share a hash", keys_share_hash);
+    ADD_TEST("handles multiple hash value types", hash_types);
+    ADD_TEST("similar usage to an unordered map", similar_to_unordered_map);
     RUN_ALL_TESTS();
 
     return EXIT_SUCCESS;
